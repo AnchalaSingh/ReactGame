992940466c4c56e1b158cea6473f7759296933bb
AnchalaSingh <anchalasingh723@gmail.com>
2024-10-22 11:35:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const questions = [
+  {
+    questionText: 'What is the capital of France?',
+    answerOptions: [
+      { answerText: 'Paris', isCorrect: true },
+      { answerText: 'London', isCorrect: false },
+      { answerText: 'Berlin', isCorrect: false },
+      { answerText: 'Rome', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Who is CEO of Tesla?',
+    answerOptions: [
+      { answerText: 'Jeff Bezos', isCorrect: false },
+      { answerText: 'Elon Musk', isCorrect: true },
+      { answerText: 'Bill Gates', isCorrect: false },
+      { answerText: 'Tony Stark', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'The iPhone was created by which company?',
+    answerOptions: [
+      { answerText: 'Apple', isCorrect: true },
+      { answerText: 'Intel', isCorrect: false },
+      { answerText: 'Amazon', isCorrect: false },
+      { answerText: 'Microsoft', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'How many Harry Potter books are there?',
+    answerOptions: [
+      { answerText: '1', isCorrect: false },
+      { answerText: '4', isCorrect: false },
+      { answerText: '6', isCorrect: false },
+      { answerText: '7', isCorrect: true },
+    ],
+  },
+];
+
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  const handleRestart = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowScore(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      {showScore ? (
+        <div className='score-section'>
+          <div className='score-text'>You scored {score} out of {questions.length}</div>
+          <button className='restart-button' onClick={handleRestart}>
+            Restart
+          </button>
+        </div>
+      ) : (
+        <div className='question-section'>
+          <div className='question-count'>
+            <span>Question {currentQuestion + 1}</span>/{questions.length}
+          </div>
+          <div className='question-text'>{questions[currentQuestion].questionText}</div>
+          <div className='answer-section'>
+            {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+              <button key={index} onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
